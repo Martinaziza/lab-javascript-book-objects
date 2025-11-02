64003d07f9cfea6f3ef68c97dd0447efe990ced6
Martina Ziza <martina.ziza@gmail.com>
2025-11-02 13:56:56 +0100

    lab done

--- src/books.js
+++ src/books.js
@@ -1,13 +1,6 @@
 // Iteration 1 | Books Array
 
 // Book 1
-// title: The Old Man and the Sea
-// pages: 128
-// author: Ernest Hemingway
-// details: {
-//    language: English
-//    description: One of Hemingway's most famous works, it tells the story of Santiago...
-// }
 
 // Book 2
 // title: The Airbnb Story
@@ -36,65 +29,141 @@
 //    description: The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.
 // }
 
-
 // Your code here:
-const booksArray = [];
-
-
-
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description:
+        "One of Hemingway’s most famous works, it tells the story of Santiago...",
+    },
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description:
+        "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+    },
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description:
+        "Educated is an account of the struggle for self-invention...",
+    },
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description:
+        "The Art of Learning takes readers through Waitzkin’s unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+    },
+  },
+];
 
 // Iteration 2 | Book Details
-function getBookDetails() {
-  // Your code here:
+function getBookDetails(obj) {
+ const objTitle = obj.title;
+ const objAuthor = obj.author;
+ const objPages = obj.pages;
 
+ return `${objTitle} - ${objAuthor} - ${objPages} pages`
+//return obj.title + " - " + obj.author + " - " + obj.pages + " pages"
 }
 
-
+console.log(getBookDetails({
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description:
+        "Educated is an account of the struggle for self-invention...",
+    }}))
 
 // Iteration 3 | Delete Language
-// Your code here:
+// Iterate over the booksArray, and delete the nested object property language from each book object.
+// Once done, console.log the booksArray array to confirm that the property has 
+// been deleted from all the book objects.
 
 
+booksArray.forEach ((book)=>{
+delete book.details.language
+})
 
+console.log(booksArray)
 
 // Iteration 4 | Estimated Reading Time
-// Your code here:
+// Iterate over the booksArray, and add a new property readingTime to each book object.
+// The value of the readingTime should be the number of minutes it will take to 
+// read the book. The number of minutes should be an integer (i.e., no decimals).
 
+// Assuming a page consists of 500 words and that the average reader reads 90 words 
+// per minute, the estimated reading time for a book can be calculated using 
+// the following formula:
 
+// reading time in minutes = (number of pages * 500) / 90
 
+// The reading time must be rounded UP to the next whole number,
+// even if the result is a small decimal. For example:
 
+// If the result is 711.1, round it up to 712.
+// If the result is 30.03, round it up to 31.
+
+  booksArray.forEach ((book)=> {
+    let key = "readingTime"
+    let value = 0;
+    book[key] = value;
+    let readingTimeCalc = (book.pages * 500)/90
+    let readingTimeRound = Math.ceil(readingTimeCalc)
+    book.readingTime = Math.ceil((book.pages * 500)/90)
+    
+
+})
+//console.log(readingTimeRound)
+ //console.log(booksArray)
+
+ 
 // Bonus: Iteration 5 | Books Dictionary
 
 /* The `dictionary` is an object containing books grouped by author. 
  The book info is stored in arrays with structure: [title, pages]. 
 */
 const dictionary = {
-    "J. K. Rowling": [
-        ["Harry Potter and the Philosopher's Stone", 223],
-        ["Harry Potter and the Chamber of Secrets", 251],
-        ["Harry Potter and the Prisoner of Azkaban", 317],
-        ["Harry Potter and the Goblet of Fire", 636],
-    ],
-    "Neal Stephenson": [
-        ["Cryptonomicon", 928],
-        ["Anathem", 1008],
-        ["Fall; or, Dodge in Hell", 896],
-    ],
-    "Malcolm Gladwell": [
-        ["Outliers", 320],
-        ["Blink", 287],
-    ],
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
 };
 
 function booksByAuthor() {
   // Your code here:
-  
 }
-
-
 
 // Bonus: Iteration 6 | Average Page Count
 function averagePageCount() {
   // Your code here:
-  
 }
